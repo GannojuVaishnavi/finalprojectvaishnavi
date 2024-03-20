94b2e719fef9b472aa01661c1952d1d1b229dfc8
Gannoju Vaishnavi <123318338+GannojuVaishnavi@users.noreply.github.com>
2024-03-20 06:19:46 +0000

    route updated

--- project-shopesmanagement/public/js.js
+++ project-shopesmanagement/public/js.js
@@ -63,7 +63,7 @@ function checkUserStatus(uname, uemail) {
     email: uemail
   }
 
-  fetch(`https://finalprojectvaishnavi-15.onrender.com/login`,
+  fetch(`https://finalprojectvaishnavi-16.onrender.com/login`,
     {
       method: "POST",
       body: JSON.stringify(data),
